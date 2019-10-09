051f2436c905afe5d05061d3f1b18b6060a04711
Nikita Belyakov <nikita.belyakov.38@ya.ru>
2019-10-09 23:00:41 +0300

    add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.maxNum = 0;
+        this.minNum = 0;
+    }
 
     setRange(min, max) {
-
+        this.minNum = min;
+        this.maxNum = max;
     }
 
     guess() {
-
+        return Math.ceil((this.maxNum - ((this.maxNum - this.minNum) / 2)));
     }
 
     lower() {
-
+        this.maxNum = Math.ceil((this.maxNum - ((this.maxNum - this.minNum) / 2)));
+        return Math.ceil((this.maxNum - ((this.maxNum - this.minNum) / 2)));
     }
 
     greater() {
-
+        this.minNum = Math.ceil((this.maxNum - ((this.maxNum - this.minNum) / 2)));
+        return Math.ceil((this.maxNum - ((this.maxNum - this.minNum) / 2)));
     }
 }
 
